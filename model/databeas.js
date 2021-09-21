14c57bfeb6bda3dfdc41f0b80b1952a8f3a622db
shaimasawai <shaima.alsawaie@gmail.com>
2021-09-21 14:59:56 -0400

    backend book

--- model/databeas.js
+++ model/databeas.js
@@ -1,6 +1,6 @@
 "use strict";
 const mongoose = require("mongoose");
-const bookschema = require("./Book model");
+const bookschema = require("./Bookmodel");
 
 const UserData = new mongoose.Schema({
   emaill: String,
--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 "use strict";
 
-require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const jsonData = require("./controller/book.controller");
-
 const app = express();
-app.use(cors());
-
-const PORT = process.env.PORT || 3001;
-const MONGO_SERVER = process.env.MONGO_DB_URL;
-mongoose.connect(`${MONGO_SERVER}/Book`, {
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT;
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
+const {
+  SearchBook,
+  CreatBook,
+  DeletBook,
+  updateBook,
+} = require("./controller/book.controller");
+mongoose.connect(`${MONGO_DB_URL}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+// to be able to capture the post req body use the below line of code
+app.use(express.json());
+app.use(cors());
+app.get("/Book-list", SearchBook);
+app.post("/create-Book", CreatBook);
+app.delete("/delete-Book/:id", DeletBook);
+app.put("/update-Book/:id", updateBook);
 
-app.get("/test", (request, response) => {
-  response.send("test request received");
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
 });
-app.get("/books", jsonData);
-
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
